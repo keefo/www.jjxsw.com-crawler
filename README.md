bdb8f6f97c88a5d7260f123e2ede75398d8da8f2
Xu Lian <lianxu@me.com>
2018-04-28 14:09:54 -0700

    Upload script and readme.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# -
-久久小说爬虫 www.jjxsw.com
+# 久久小说爬虫 
+
+A crawler downloads txt book from www.jjxsw.com
+
+# Install scrapy
+
+```bash
+pip install scrapy
+```
+
+or 
+
+```bash
+pip3 install scrapy
+```
+
+# Run Spider
+
+```bash
+scrapy runspider spider.py
+```
+
+Txt books will be saved in books folder.
+
---
+++ spider.py
@@ -0,0 +1,59 @@
+import scrapy
+import re
+import json
+import os.path
+
+from scrapy.selector import Selector
+try:
+    from scrapy.spiders import Spider
+except:
+    from scrapy.spiders import BaseSpider as Spider
+from scrapy.utils.response import get_base_url
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor as sle
+from scrapy.item import Item, Field
+
+class TxtItem(Item):
+    name = Field()
+    url = Field()
+
+class BlogSpider(scrapy.Spider):
+    name = 'blogspider'
+    start_urls = ['http://www.jjxsw.com/']
+ 
+
+    def parse(self, response):
+        items = []
+        for link in response.xpath("//a[starts-with(@href, '/txt/')]"):
+             item = TxtItem()
+             name = link.xpath('text()').extract_first();
+             item['name'] = name
+             item['url'] = link.xpath('@href').extract()
+             items.append(item)
+             print(item)
+             yield response.follow(link, self.parse_book_page, meta={'name': name})
+             
+    def parse_book_page(self, response):
+        for link in response.xpath("//li[@class='downAddress_li']/a"):
+            yield {'download': link.extract()}
+            yield response.follow(link, self.parse_download_page, meta=response.meta)
+            break
+    
+    def parse_download_page(self, response):
+        for link in response.css("a.green"):
+            title = link.xpath("text()").extract_first()
+            if title.startswith( 'TXT电子书下载地' ):
+                yield {'final_download': link.extract()}
+                yield response.follow(link, self.save_text, meta=response.meta)                
+                #return link
+                break
+
+    def save_text(self, response):
+        path = response.url.split('/')[-1]
+        extension = os.path.splitext(path)[1]
+
+        self.logger.info('Saving Txt %s', path)
+        name = "books/"+response.meta["name"]+extension
+        with open(name, 'wb') as f:
+            f.write(response.body)
+
